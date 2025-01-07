4287ece496b3d167346ce7d53224885d46946161
Caleb Xu <calxu@iu.edu>
2025-01-07 15:17:23 -0500

    added react js

---
+++ app.js
@@ -0,0 +1,19 @@
+//Modules
+const express = require("express");
+const path = require("path");
+
+//Init
+const app = express();
+const PORT = process.env.PORT || 80;
+
+//Error catching
+app.listen(PORT, (error) =>{
+    if(!error){
+        console.log("Server is successfully running, and App is listening on port " + PORT);
+    }
+    else{
+        console.log("Error occured, server can't start", error);
+    }
+}
+);
+
