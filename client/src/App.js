1ecce10c2773809750f823029ed6242367814713
Caleb Xu <calxu@iu.edu>
2025-01-07 16:11:04 -0500

    removed react

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,19 @@
+import { useEffect, useState } from 'react'
+
 function MyButton() {
+    
+    const [backendData, setBackendData] = useState([{}])
+
+    useEffect(() => {
+        fetch("/api").then(
+            response => response.json()
+        ).then(
+            data => {
+                setBackendData(data)
+            }
+        )
+    }, [])
+
     return (
         <button>
             I'm a button
